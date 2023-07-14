17bd0e7acb6b4cce6bafeee654cc4ec04a670159
Andres Zurita <andres.ipschile@gmail.com>
2023-07-13 20:44:50 -0400

    ahora probando puerto

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express()
 app.get('/',(req, res)=>{
     console.log('Petición recibida')
 
-    res.status(200).send('<h1>Hola mundo con NODEMON!</h1>')
+    res.status(200).send('<h1>Hola mundo</h1>')
 })
 
 const PORT = process.env.PORT
 app.listen(PORT,() => {
-    console.log('Servidor escuchando en el puerto',{PORT})
+    console.log('Servidor escuchando en el puerto', {PORT})
 })
